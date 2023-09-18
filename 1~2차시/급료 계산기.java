8453dc9f4239a8bb87afe60de7b120ce66fd0fc8
Blue_bla <blue__bla@naver.com>
2023-09-19 00:36:00 +0900

    Update

---
+++ 1~2차시/급료 계산기.java
@@ -0,0 +1,20 @@
+{
+        Scanner keyboard = new Scanner(System.in);
+        int money, pay, time, salary;
+        final int WEEK = 7;
+
+        System.out.print("현재 지갑에 얼마가 있나요? :");
+        money = keyboard.nextInt();
+        System.out.print("시간당 임금이 얼마인가요? :");
+        pay = keyboard.nextInt();
+        System.out.print("몇시간 일했나요? :");
+        time = keyboard.nextInt();
+
+        salary = pay * time * WEEK;
+
+        System.out.printf("\n현재 지갑의 돈 : %,d 원\n", money);
+        System.out.printf("시간당 임금 : %,d 원\n", pay);
+        System.out.printf("하루에 %d시간 일했습니다\n", time);
+        System.out.printf("일주일치 급료 %,d 원\n", salary);
+        System.out.printf("나의 총 금액 : %,d 원\n", salary + money);
+    }
